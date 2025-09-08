f11cd8f7474bc8c9697ecfdfac745a11f93bb1ae
Rishab Nuguru <rnuguru@colostate.edu>
2025-09-07 20:25:36 -0600

    More Space

--- apply-patches.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Apply patch to pybind11 CMakeLists.txt to fix cmake version
-echo "Applying pybind11 CMake version patch..."
-patch -p0 < patches/pybind11-cmake-version.patch
-
-echo "Patches applied successfully!" 
